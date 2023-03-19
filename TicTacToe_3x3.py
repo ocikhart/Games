7419a62bf3fdf668d9844aa0462005b3c3a16b10
Ondrej Cikhart <ocikhart@outlook.com>
2023-03-19 12:12:03 +0100

    Code refactoring

--- TicTacToe_3x3.py
+++ TicTacToe_3x3.py
@@ -2,209 +2,14 @@ import numpy as np
 from itertools import product
 import pandas as pd
 import tensorflow as tf
-import random
+import TicTacToe as ttt
 from matplotlib import pyplot as plt
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense, InputLayer
 from tensorflow.python.keras.losses import MSE
 from tensorflow.python.keras.optimizers import adam_v2 as Adam
 
-
-class TicTacToe():
-    def __init__(self,player = 'X',reward_type ='goal_reward'):
-        '''
-        player: Role agent should play. If X, agent has the first turn else agent has second turn
-        reward_type: 'goal_reward' or 'action_penalty'
-        '''
-        self.board = np.array(['__']*9).reshape(3,3)
-        self.reward_type = reward_type
-        self.winning_sequence = None #Keep track of winning move made by agent
-        self.first_move = None #Keep track of first move made by agent
-        if player == 'X':
-            self.me ='X'
-            self.id = 1
-            self.opponent = 'O'
-        else:
-            self.me = 'O'
-            self.id = 2
-            self.opponent = 'X'
-     
-        self.game_over = False #Flag indicating whether game is over
-        # Mapping of action representation in board to action representation in tuple 
-        self.b_to_s = {'__':0,'X':1,'O':2} 
-        # Mapping of action representation in tuple to action representation in board
-        self.s_to_b = {0:'__',1:'X',2:'O'} 
-        
-        #Create mapping from 2D position in board to 1D position in tuple
-        positions = self.available_positions()
-        self.b2_to_s1 = {position:i for (i,position) in enumerate(positions)}
-        
-        #Create mapping from 1D position in tuple to 2D position in board 
-        self.s1_to_b2 = {i:position for (i,position) in enumerate(positions)}
-        
-        #State the current player is in
-        self.starting_state = self.board_to_state()
-        
-        #Initialize all possible states of the game
-        l_o_l = [list(range(3)) for _ in range(9)]
-        states = set(product(*l_o_l))
-        
-
-        
-        #Player X states include states with odd number of blanks and both players have occupied equal number of slots
-        #Player O players after Player X, so player O states include states with even number of blanks and where
-        #player X has occupied one more slot than player O
-        playerX_states = {state for state in states if (state.count(0)%2 == 1 and state.count(1)==state.count(2))} #
-        playerO_states =  {state for state in states if (state.count(0)%2 == 0 and state.count(1)==(state.count(2)+1))}
-        
-        #States 
-        #self.board_full_states = {state for state in states if state.count(0)==0}
-        if player == 'X':
-            self.my_states = playerX_states
-        else:
-            self.my_states = playerO_states
-          
-    
-    def reset_board(self):
-        "Function to reset game and reset board to starting state"
-        self.board = np.array(['__']*9).reshape(3,3)
-        self.starting_state = self.board_to_state()
-        self.game_over = False
-        self.winning_sequence = None
-        self.first_move = None
-    
-    def show_board(self):    
-        "Shows board as a pandas dataframe"
-        return pd.DataFrame(self.board)
-    
-    def board_to_state(self):
-        "Convert a board to a state in tuple format"
-        return tuple([self.b_to_s[x] for x in np.ravel(self.board)])
-    
-    @staticmethod
-    def possible_actions(state):
-        "Return possible actions given a state"
-        return [i for i,x  in enumerate(state) if x ==0]
-    
-
-        
-    def is_game_over(self):
-        "Function to check if game is over"
-        if not np.any(self.board == '__') :
-            self.game_over = True
-            
-        return self.game_over
-    
-    def available_positions(self):
-        "Return available positions on the board"
-        x,y = np.where(self.board =='__')
-        return[(x,y) for x,y in zip(x,y)]
-    
-    
-    def win(self,player):
-        "Check if player won the game and record the winning sequence"
-        if np.all(self.board[0,:] == player):
-            self.winning_sequence = 'R1'
-        elif np.all(self.board[1,:] == player): 
-            self.winning_sequence = 'R2'
-        elif np.all(self.board[2,:] == player):
-            self.winning_sequence = 'R3'
-        elif np.all(self.board[:,0] == player):
-            self.winning_sequence = 'C1'
-        elif np.all(self.board[:,1] == player):
-            self.winning_sequence = 'C2'
-        elif np.all(self.board[:,2] == player):
-            self.winning_sequence = 'C3'
-        elif np.all(self.board.diagonal()==player):
-            self.winning_sequence = 'D1'
-        elif  np.all(np.fliplr(self.board).diagonal()==player):
-            self.winning_sequence = 'D2'
-        else:
-            return False
-        
-        return True
-    
-    
-    def my_move(self,position):
-        "Fills out the board in the given position with the action of the agent"
-        
-        assert position[0] >= 0 and position[0] <= 2 and position[1] >= 0 and position[1] <= 2 , "incorrect position"
-        assert self.board[position] == "__" , "position already filled"
-        assert np.any(self.board == '__') , "Board is complete"
-        assert self.win(self.me) == False and self.win(self.opponent)== False , " Game has already been won"
-        self.board[position] = self.me
-        
-        I_win = self.win(self.me)
-        opponent_win = self.win(self.opponent)
-        
-        if self.reward_type == 'goal_reward':
-            if I_win:
-                self.game_over = True
-                return 1
-            
-            elif opponent_win:
-                self.game_over = True
-                return -1
-            
-            else:
-                return 0
-            
-        elif self.reward_type == 'action_penalty':
-            if I_win:
-                self.game_over = True
-                return 0
-            
-            elif opponent_win:
-                self.game_over = True
-                return -10
-            
-            else:
-                return -1
-    
-    def opponent_move(self,position):
-        "Fills out the board in the given position with the action of the opponent"
-        assert position[0] >= 0 and position[0] <= 2 and position[1] >= 0 and position[1] <= 2 , "incorrect position"
-        assert self.board[position] == "__" , "position already filled"
-        assert np.any(self.board == '__') , "Board is complete"
-        assert self.win(self.me) == False and self.win(self.opponent)== False , " Game has already been won"
-        self.board[position] = self.opponent
-            
-    
-    def pick_best_action(self,Q,action_type,eps=None):
-        '''Given a Q function return optimal action
-        If action_type is 'greedy' return best action with ties broken randomly else return epsilon greedy action
-        '''
-        #Get possible actions
-        current_state = self.board_to_state()
-        actions =  self.possible_actions(current_state)
-        
-        best_action = []
-        best_action_value = -np.Inf
-        
-        for action in actions:
-            Q_s_a = Q(np.asarray([current_state]),action,self.id)
-            if Q_s_a == best_action_value:
-                best_action.append(action)
-            elif Q_s_a > best_action_value:
-                best_action = [action]
-                best_action_value = Q_s_a
-        best_action = random.choice(best_action)
-
-        if action_type == 'greedy':
-            return self.s1_to_b2[best_action]
-        else:
-            assert eps != None , "Include epsilon parameter"
-            n_actions =len(actions) #No of legal actions 
-            p = np.full(n_actions,eps/n_actions)
-            #Get index of best action
-            best_action_i = actions.index(best_action)
-            p[best_action_i]+= 1 - eps
-            return self.s1_to_b2[np.random.choice(actions,p=p)]
-    
-def Q_random(state, action, player):
-    return 0.
-
-
+   
 def Q_NN(state, action, player):
     print(state)
     assert action in range(len(state[0])), "incorrect position"
@@ -297,7 +102,7 @@ def agent_learn(experiences, gamma):
     update_target_network(q_network, target_q_network)
 
 
-def play_games(n, player_X, Q_X, player_O, Q_O):
+def play_games(n, player_X, player_O):
     """
     Simulates N games
     
@@ -313,11 +118,11 @@ def play_games(n, player_X, Q_X, player_O, Q_O):
     """
     for j in range(n):
         game_over = False
-        TicTacToe.reset_board(player_X)
-        TicTacToe.reset_board(player_O)
+        ttt.TicTacToe.reset_board(player_X)
+        ttt.TicTacToe.reset_board(player_O)
         i = 1
         while True:
-            action_X = t_board_X.pick_best_action(Q_X, 'greedy')
+            action_X = t_board_X.pick_best_action('greedy')
             print(f"Match #{j+1} Round #{i} PLAYER X: {action_X}")
             res_X = t_board_X.my_move(action_X)
             print(t_board_X.show_board())
@@ -326,10 +131,10 @@ def play_games(n, player_X, Q_X, player_O, Q_O):
             else:
                 print(f"WINNING MOVE ---> Winning Sequence {t_board_X.winning_sequence}")
                 break
-            if not np.any(t_board_X.board == '__'):
+            if not np.any(t_board_X.board == 0):
                 print(f"DRAW")
                 break
-            action_O = t_board_O.pick_best_action(Q_O, 'greedy')
+            action_O = t_board_O.pick_best_action('greedy')
             print(f"Match #{j+1} Round #{i} PLAYER O: {action_O}")
             res_X = t_board_O.my_move(action_O)
             print(t_board_O.show_board())
@@ -338,7 +143,7 @@ def play_games(n, player_X, Q_X, player_O, Q_O):
             else:
                 print(f"WINNING MOVE ---> Winning Sequence {t_board_O.winning_sequence}")
                 break
-            if not np.any(t_board_O.board == '__'):
+            if not np.any(t_board_O.board == 0):
                 print(f"DRAW")
                 break
             i += 1
@@ -355,8 +160,8 @@ E_DECAY = 0.995  # ε-decay rate for the ε-greedy policy.
 E_MIN = 0.01  # Minimum ε value for the ε-greedy policy.
 
 
-t_board_X = TicTacToe(player = 'X',reward_type ='goal_reward')
-t_board_O = TicTacToe(player = 'O',reward_type ='goal_reward')
+t_board_X = ttt.TicTacToe(player = 1,reward_type ='goal_reward')
+t_board_O = ttt.TicTacToe(player = 2,reward_type ='goal_reward')
 
 tf.random.set_seed(SEED)
 # Create the Q-Network
@@ -376,6 +181,14 @@ target_q_network = Sequential([
     ])
 
 optimizer = Adam.Adam(ALPHA)
-# y = q_network(np.array([[1,0,0,1,0,0,1,0,0]]))
-# print(y)
-play_games(2, t_board_X, Q_NN, t_board_O, Q_random)
+# y = q_network(np.array([(1,0,0,1,0,0,1,0,0)]))
+actions = [i for i,x  in enumerate(np.ravel(t_board_X.board)) if x ==0]
+print(actions)
+states = np.tile(np.ravel(t_board_X.board), (len(actions),1))
+print(states)
+for i,action in enumerate(actions):
+    states[i][action] = 1
+print(states)
+print(t_board_X.Q(states))
+
+play_games(2, t_board_X, t_board_O)
